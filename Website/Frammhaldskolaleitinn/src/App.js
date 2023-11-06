9ca085b7c8aff4ed1dd4f27a3f8bf9f2a258c0c5
Accent8 <hreimur69@gmail.com>
2023-11-06 11:00:02 +0000

    breytti nafni a dir

---
+++ Website/Frammhaldskolaleitinn/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header><img src={logo} alt="Logo" className='logo' />Frammhaldsskóla leitinn</header>
+  );
+}
+
+export default App;
