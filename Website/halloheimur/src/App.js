93b3a92051872412bad7c0be38018b617c37f9e0
Accent8 <hreimur69@gmail.com>
2023-11-06 10:10:16 +0000

    byrjun a sidu, hallo heimur

---
+++ Website/halloheimur/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className="Center">Hallo Heimur</h1>
+      <h2 className="Center">React hella whack</h2>
+    </div>
+  );
+}
+
+export default App;
