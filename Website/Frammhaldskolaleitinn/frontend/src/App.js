f4985964f9d6e3b169defad5a0c9017962041f24
Accent8 <hreimur69@gmail.com>
2023-11-14 13:59:02 +0000

    publishing branch

---
+++ Website/Frammhaldskolaleitinn/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [data, setdata] = useState ({
+    name: '',
+    age: 0,
+        date: "",
+        programming: "",
+  })
+  
+  return (
+    
+  );
+}
+
+export default App;
