4ebbfad62ce57c69cd6283e88d8d2ada7e038e44
Hreimur Logi <70168436+Accent8@users.noreply.github.com>
2023-12-05 13:52:17 +0000

    Delete Website/Frammhaldskolaleitinn/backend directory
    erum að baila á flask og vs code er bugging og leyfir mér ekki að eyða þar

--- Website/Frammhaldskolaleitinn/backend/RestAPI/config.py
+++
@@ -1,7 +0,0 @@
-USER = 'admin'
-PASSWORD = '3fac9e934892c683b45e41dded567df80185d5c6c00751de'
-HOST = '127.0.0.1'
-DB = 'framhaldsskolaleit'
-AUTH = 'mysql_native_password'
-
-
--- Website/Frammhaldskolaleitinn/backend/mongotesting/mongopass.py
+++
@@ -1,1 +0,0 @@
-mongopass = "mongodb+srv://Viktorfrans:Dbtenging@cluster0.jvmjfif.mongodb.net/?retryWrites=true&w=majority"
