7e6e788aabbca502164309d324ff094caefd11fd
Hreimur Logi <70168436+Accent8@users.noreply.github.com>
2023-12-11 09:24:44 +0000

    Lagaði searchbar v15

--- Website/Frammhaldskolaleitinn/server/server.js
+++ Website/Frammhaldskolaleitinn/server/server.js
@@ -5,7 +5,7 @@ const port = 3001;
 
 app.use(cors());
 app.use(express.json());
-
+// þetta er bara fyrir mig (hreim) þar sem tenginn við mongo virkar ekki hjá mér
 app.get('/api/data',(req,res) => {
     const jsonData = require('/Users/accent/Documents/Skóli/VefForritun2/Verkefni/Verkefni/Website/Frammhaldskolaleitinn/server/skolar.json')
     res.json(jsonData)
@@ -53,36 +53,4 @@ app.get('/api/data', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
-})
- async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    const findResult = await myColl.find({});
-    for await (const doc of findResult) {
-    console.log(doc);
-}
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-const findResult = myoll.find({});
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end();
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
-*/
+}) */
